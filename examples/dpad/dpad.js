bb44ff61fecfa745a6df3cd8f11c9258df5c7df8
Francois Laberge <francoislaberge@gmail.com>
2012-10-23 10:27:10 -0400

    First pass at reference DPad implementation

--- examples/dpad/dpad.js
+++ examples/dpad/dpad.js
@@ -4,44 +4,153 @@
         dpadOffsetY = 144,
         arrowWidth = 130,
         arrowHeight = 114,
+        // Inner hit rects
+        smallerDpadOffsetX = dpadOffsetX+114,
+        smallerDpadOffsetY = dpadOffsetY+114,
         smallerArrowWidth = 46,
         smallerArrowHeight = 42,
-        drawMode = "hitrects",  // "normal" Draws Regularly
-                                // "hitrects" Makes the controller display the HitRects
-                                // of all Rects
-                                
-        states = ['left','right','up','down'],
-
+        // Draw Mode
+        //    "normal" Draws Regularly
+        //    "hitrects" Makes the controller display the HitRects of all Rects
+        drawMode = "normal",
+        // 
+        states = {
+          // State that is mutated as BM events come in
+          internal:{
+            left:0,
+            right:0,
+            up:0,
+            down:0
+          },
+          current:{
+            left:false,
+            right:false,
+            up:false,
+            down:false
+          },
+          // Shadow state is updated each game loop
+          // to hold the previous states of the dpad
+          // buttons
+          prev:{
+            left:false,
+            right:false,
+            up:false,
+            down:false
+          }
+        },
         // All arrow positions are relative to the dpad's
         // origin (dpadOffsetX,dpadOffsetY)
         arrows = {
+          upleft:{
+            x:          0+dpadOffsetX,
+            y:          0+dpadOffsetY,
+            width:      114,
+            height:     114
+          },
           up:{
-            x:          arrowHeight,
-            y:          0,
-            width:      arrowWidth,
-            height:     arrowHeight
+            x:          114+dpadOffsetX,
+            y:          0+dpadOffsetY,
+            width:      130,
+            height:     114
           },
-          down:{
-            x:          arrowHeight,
-            y:          arrowHeight+arrowWidth,
-            width:      arrowWidth,
-            height:     arrowHeight
-          },
-          left:{
-            x:          0,
-            y:          arrowHeight,
-            width:      arrowHeight,
-            height:     arrowWidth
+          upright:{
+            x:          244+dpadOffsetX,
+            y:          0+dpadOffsetY,
+            width:      114,
+            height:     114
           },
           right:{
-            x:          arrowHeight+arrowWidth,
-            y:          arrowHeight,
-            width:      arrowHeight,
-            height:     arrowWidth
+            x:          244+dpadOffsetX,
+            y:          114+dpadOffsetY,
+            width:      114,
+            height:     130
+          },
+          downright:{
+            x:          244+dpadOffsetX,
+            y:          244+dpadOffsetY,
+            width:      114,
+            height:     114
+          },
+          down:{
+            x:          114+dpadOffsetX,
+            y:          244+dpadOffsetY,
+            width:      130,
+            height:     114
+          },
+          downleft:{
+            x:          0+dpadOffsetX,
+            y:          244+dpadOffsetY,
+            width:      114,
+            height:     114
+          },
+          left:{
+            x:          0+dpadOffsetX,
+            y:          114+dpadOffsetY,
+            width:      114,
+            height:     130
+          },
+          
+          // Small
+          upleftsmall:{
+            x:          0+smallerDpadOffsetX,
+            y:          0+smallerDpadOffsetY,
+            width:      42,
+            height:     42
+          },
+          upsmall:{
+            x:          42+smallerDpadOffsetX,
+            y:          0+smallerDpadOffsetY,
+            width:      46,
+            height:     42
+          },
+          uprightsmall:{
+            x:          88+smallerDpadOffsetX,
+            y:          0+smallerDpadOffsetY,
+            width:      42,
+            height:     42
+          },
+          rightsmall:{
+            x:          88+smallerDpadOffsetX,
+            y:          42+smallerDpadOffsetY,
+            width:      42,
+            height:     46
+          },
+          downrightsmall:{
+            x:          88+smallerDpadOffsetX,
+            y:          88+smallerDpadOffsetY,
+            width:      42,
+            height:     42
+          },
+          downsmall:{
+            x:          42+smallerDpadOffsetX,
+            y:          88+smallerDpadOffsetY,
+            width:      46,
+            height:     42
+          },
+          downleftsmall:{
+            x:          0+smallerDpadOffsetX,
+            y:          88+smallerDpadOffsetY,
+            width:      42,
+            height:     42
+          },
+          leftsmall:{
+            x:          0+smallerDpadOffsetX,
+            y:          42+smallerDpadOffsetY,
+            width:      42,
+            height:     46
           }
         };
   
+    // Add Resources
     images.push('dpad.png');
+    images.push('dpad-upleft.png');
+    images.push('dpad-up.png');
+    images.push('dpad-upright.png');
+    images.push('dpad-right.png');
+    images.push('dpad-downright.png');
+    images.push('dpad-down.png');
+    images.push('dpad-downleft.png');
+    images.push('dpad-left.png');
     images.push('hitrect.png');
     images.push('transparent.png');
     
@@ -67,19 +176,22 @@
       var elem = {
             type:       "button",
             handler:    arrow
-          };
+          },
+          // Strip off the 'small' part of the arrow's field name
+          // so we know which state image it goes with
+          realName = arrow.replace('small');
     
       // Normal Draw Mode?
       if(drawMode=="normal"){
         elem.imageUp    = getImageIndex('transparent.png');
-        elem.imageDown  = getImageIndex(arrow+'.png');
+        elem.imageDown  = getImageIndex('dpad-'+realName+'.png');
         elem.x          = dpadOffsetX;
         elem.y          = dpadOffsetY;
         elem.width      = 358;
         elem.height     = 358;
         elem.hitRect    = {
-          x: arrows[arrow].x+dpadOffsetX,
-          y: arrows[arrow].y+dpadOffsetY,
+          x: arrows[arrow].x,
+          y: arrows[arrow].y,
           width: arrows[arrow].width,
           height: arrows[arrow].height
         };
@@ -88,8 +200,8 @@
       else{
         elem.imageUp    = getImageIndex('hitrect.png');
         elem.imageDown  = getImageIndex('transparent.png');
-        elem.x          = arrows[arrow].x+dpadOffsetX;
-        elem.y          = arrows[arrow].y+dpadOffsetY;
+        elem.x          = arrows[arrow].x;
+        elem.y          = arrows[arrow].y;
         elem.width      = arrows[arrow].width;
         elem.height     = arrows[arrow].height;
       }
@@ -105,19 +217,88 @@
           return i;
         }
       }
-    }  
-  }
-  
-  
-  
-  bm.onInvocation(function(invoke, deviceId){
-    var keyDown = invoke.parameters[0].Value=="down";
-    if(keyDown){
-      console.log(invoke.methodName+' is down.');
-    } else {
-      console.log(invoke.methodName+' is up.');
     }
-  });
+    
+    this.getState = function(which){
+      return states.current[which];
+    }
+    
+    this.getPrevState = function(which){
+      return states.prev[which];
+    }
+    
+    this.getCurrentStateImage = function(){
+      var str = "";
       
-
+      // Up/Down
+      if(states.current.up){
+        str+="up";
+      } else if(states.current.down){
+        str+="down";
+      }
+      
+      // Left/Right
+      if(states.current.left){
+        str+="left"
+      } else if(states.current.right){
+        str+="right"
+      }
+      
+      // Is anything even pressed?
+      if(str==""){
+        str = "dpad.png";
+      } else {
+        str = "dpad-"+str+".png";
+      }
+      
+      console.log(str);
+      return str;
+    }
+    
+    // Callback register for dpad events
+    this.on = function(cb){
+      this.cb = cb;
+    }
+    
+    // Clients of this class should call this at the beginning
+    // of their game loops
+    this.update = function(){
+      
+      // Update the prev state with the new state
+      // and generate events for any state that has
+      // changed
+      for(s in states.current){
+        
+        // Copy Old Current State to Previous
+        states.prev[s] = states.current[s];
+        
+        // Copy Internal State into Current
+        states.current[s] = states.internal[s]!=0;
+        
+        // Generate events for changes in states
+        if( this.cb!==undefined&&
+            states.current[s]!=states.prev[s]){
+          this.cb(s, states.current[s]);
+        }
+      }
+    }
+    
+    bm.onInvocation(function(invoke, deviceId){
+      var keyDown = invoke.parameters[0].Value=="down",
+          realName = invoke.methodName.replace('small');
+     
+      // Figure out which states this button affects
+      for(s in states.current){
+        if(realName.search(s)!=-1){
+          
+          // Do reference count of fingers over a button
+          if(keyDown){
+            states.internal[s] += 1;
+          } else {
+            states.internal[s] -= 1;
+          }
+        }
+      }
+    });
+  }
 })();
