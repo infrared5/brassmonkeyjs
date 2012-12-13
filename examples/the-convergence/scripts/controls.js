6263e13090fc3ea69584a15b93eec7307878f706
Francois Laberge <francoislaberge@gmail.com>
2012-12-13 12:50:38 -0500

    Cleaning up Flash Builder files. With cleaner ignores. Will writeup examples in subsequent commits

--- examples/the-convergence/scripts/controls.js
+++ examples/the-convergence/scripts/controls.js
@@ -15,7 +15,7 @@ bm.init({
   // Location of the brassmonkey.swf the SDK depends on.
   // Coming soon this dependency will be removed for platforms
   // like Mobile Safari that don't support flash.
-  swfURL:"../../src/swf/bin/brassmonkey.swf",
+  swfURL:"../../swf/sdk/bin/brassmonkey.swf",
   
   // Describe the controller's design/layout
   design: {
